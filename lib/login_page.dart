718381c1da6e8ad60b7bde9bac57ebbcf7789255
jjaeni <jjaeeun3203@naver.com>
2025-12-20 22:08:33 +0900

    update "Login page"

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      backgroundColor: Colors.white
+      ,
+      appBar:AppBar(
+        title: const Text('Login'),
+      ),
+    body: Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+
+          // TextField of ID
+          TextField(
+            decoration: const InputDecoration(
+              labelText: 'ID',
+              border: OutlineInputBorder(), // outer square line
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          // TextField of PW
+          TextField(
+            obscureText: true, // hide PW
+            decoration: const InputDecoration(
+              labelText: 'Password',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ],
+      ),
+    ),
+    );
   }
 }
