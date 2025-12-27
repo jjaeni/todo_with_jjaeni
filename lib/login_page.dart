ba4c6e928f0613e39eb0e3ea9f4051fbb1d29162
jjaeni <jjaeeun3203@naver.com>
2025-12-27 17:22:27 +0900

    Updated to "login_page.dart" with temporary login logic.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,58 +10,70 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
-  // id/pw Controller
+  // Declare a id/pw Controller
   final TextEditingController idController = TextEditingController();
   final TextEditingController pwController = TextEditingController();
 
   void _login() {
+    // Change a value in variable only.
     final id = idController.text;
     final pw = pwController.text;
 
-  // Temporary logic of login (will change temporary login to DB/API)
-  if (id == 'test' && pw == '1234') {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const HomePage()),
-    );
-  } else {
-    // Will update soon.
+    // Temporary logic of login (will change temporary login to DB/API)
+    if (id == 'test' && pw == '1234') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    } else {
+      // When you fill the wrong ID/PW, display this snack bar in the screen.
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('ID 또는 PW가 틀렸습니다.'))
+      );
+    }
   }
-  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white
-      ,
-      appBar:AppBar(
-        title: const Text('Login'),
-      ),
-    body: Padding(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        children: [
+      appBar:AppBar(title: const Text('Login')),
+      backgroundColor: const Color.fromARGB(255, 230, 230, 230),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
 
-          // TextField of ID
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'ID',
-              border: OutlineInputBorder(), // outer square line
+            // TextField of ID
+            TextField(
+              controller: idController,
+              decoration: const InputDecoration(
+                labelText: 'ID',
+                border: OutlineInputBorder(), // outer square line
+              ),
             ),
-          ),
 
-          const SizedBox(height: 20),
+            const SizedBox(height: 12),
 
-          // TextField of PW
-          TextField(
-            obscureText: true, // hide PW
-            decoration: const InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(),
+            // TextField of PW
+            TextField(
+              controller: pwController,
+              obscureText: true, // hide PW
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
             ),
-          ),
-        ],
+
+            // Press button of Login
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: const Text('login'))
+          ],
+        ),
       ),
-    ),
     );
   }
 }
